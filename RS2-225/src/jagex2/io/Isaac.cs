270f8e63b8a84285fb73c0836f66b301e489be8e
Tyler Bochard <zeruth09@gmail.com>
2024-08-12 03:21:12 -0400

    develop jagex2/io
    adds:
    Isaac
    Packet

---
+++ RS2-225/src/jagex2/io/Isaac.cs
@@ -0,0 +1,191 @@
+﻿namespace RS2_225.jagex2.io;
+
+public class Isaac
+{
+    private int count = 0;
+    private uint[] rsl = new uint[256];
+    private uint[] mem = new uint[256];
+    private uint a = 0;
+    private uint b = 0;
+    private uint c = 0;
+
+    public Isaac(uint[] seed)
+    {
+        for (var i = 0; i < seed.Length; i++)
+        {
+            rsl[i] = seed[i];
+        }
+        init();
+    }
+
+    public int nextInt()
+    {
+        if (count-- == 0) {
+            isaac();
+            count = 255;
+        }
+        return (int) rsl[this.count];
+    }
+
+    private void init()
+    {
+        var a = 0x9e3779b9;
+        var b = 0x9e3779b9;
+        var c = 0x9e3779b9;
+        var d = 0x9e3779b9;
+        var e = 0x9e3779b9;
+        var f = 0x9e3779b9;
+        var g = 0x9e3779b9;
+        var h = 0x9e3779b9;
+
+        for (var i = 0; i < 4; i++)
+        {
+            a ^= b << 11;
+            d += a;
+            b += c;
+            b ^= c >>> 2;
+            e += b;
+            c += d;
+            c ^= d << 8;
+            f += c;
+            d += e;
+            d ^= e >>> 16;
+            g += d;
+            e += f;
+            e ^= f << 10;
+            h += e;
+            f += g;
+            f ^= g >>> 4;
+            a += f;
+            g += h;
+            g ^= h << 8;
+            b += g;
+            h += a;
+            h ^= a >>> 9;
+            c += h;
+            a += b;
+        }
+
+        for (var i = 0; i < 256; i++)
+        {
+            a += rsl[i];
+            b += rsl[i + 1];
+            c += rsl[i + 2];
+            d += rsl[i + 3];
+            e += rsl[i + 4];
+            f += rsl[i + 5];
+            g += rsl[i + 6];
+            h += rsl[i + 7];
+
+            a ^= b << 11;
+            d += a;
+            b += c;
+            b ^= c >>> 2;
+            e += b;
+            c += d;
+            c ^= d << 8;
+            f += c;
+            d += e;
+            d ^= e >>> 16;
+            g += d;
+            e += f;
+            e ^= f << 10;
+            h += e;
+            f += g;
+            f ^= g >>> 4;
+            a += f;
+            g += h;
+            g ^= h << 8;
+            b += g;
+            h += a;
+            h ^= a >>> 9;
+            c += h;
+            a += b;
+
+            mem[i] = a;
+            mem[i + 1] = b;
+            mem[i + 2] = c;
+            mem[i + 3] = d;
+            mem[i + 4] = e;
+            mem[i + 5] = f;
+            mem[i + 6] = g;
+            mem[i + 7] = h;
+        }
+
+        for (var i = 0; i < 256; i++)
+        {
+            a += mem[i];
+            b += mem[i + 1];
+            c += mem[i + 2];
+            d += mem[i + 3];
+            e += mem[i + 4];
+            f += mem[i + 5];
+            g += mem[i + 6];
+            h += mem[i + 7];
+
+            a ^= b << 11;
+            d += a;
+            b += c;
+            b ^= c >>> 2;
+            e += b;
+            c += d;
+            c ^= d << 8;
+            f += c;
+            d += e;
+            d ^= e >>> 16;
+            g += d;
+            e += f;
+            e ^= f << 10;
+            h += e;
+            f += g;
+            f ^= g >>> 4;
+            a += f;
+            g += h;
+            g ^= h << 8;
+            b += g;
+            h += a;
+            h ^= a >>> 9;
+            c += h;
+            a += b;
+
+            mem[i] = a;
+            mem[i + 1] = b;
+            mem[i + 2] = c;
+            mem[i + 3] = d;
+            mem[i + 4] = e;
+            mem[i + 5] = f;
+            mem[i + 6] = g;
+            mem[i + 7] = h;
+        }
+        
+        isaac();
+        count = 256;
+    }
+
+    private void isaac()
+    {
+        c++;
+        b += c;
+        for (var i = 0; i < 256; i++)
+        {
+            var x = this.mem[i];
+
+            var mem = i & 3;
+            if (mem == 0) {
+                a ^= a << 13;
+            } else if (mem == 1) {
+                a ^= a >>> 6;
+            } else if (mem == 2) {
+                a ^= a << 2;
+            } else if (mem == 3) {
+                a ^= a >>> 16;
+            }
+
+            a += this.mem[(i + 128) & 0xff];
+
+            uint y;
+            this.mem[i] = y = this.mem[(x >>> 2) & 0xff] + a + b;
+            rsl[i] = b = this.mem[((y >>> 8) >>> 2) & 0xff] + x;
+        }
+    }
+}
---
+++ RS2-225/src/jagex2/io/Packet.cs
@@ -0,0 +1,179 @@
+﻿using System.Data;
+using RS2_225.jagex2.datastruct;
+
+namespace RS2_225.jagex2.io;
+
+public class Packet : Hashable
+{
+    private static uint CRC32_POLYNOMIAL = 0xedb88320;
+    
+    private static int[] crctable = new int[256];
+    private static ulong[] bitmask = new ulong[33];
+    
+    private static LinkList cacheMin = new();
+    private static LinkList cacheMid = new();
+    private static LinkList cacheMax = new();
+    
+    private static int cacheMinCount = 0;
+    private static int cacheMidCount = 0;
+    private static int cacheMaxCount = 0;
+
+    static Packet()
+    {
+        for (var i = 0; i < 32; i++)
+        {
+            bitmask[i] = (uint)(1 << i) - 1;
+        }
+        bitmask[32] = 0xffffffff;
+
+        for (var i = 0; i < 256; i++)
+        {
+            var remainder = i;
+            for (var j = 0; j < 8; j++)
+            {
+                if ((remainder & 1) == 1) {
+                    remainder = (int)((remainder >>> 1) ^ CRC32_POLYNOMIAL);
+                } else {
+                    remainder >>>= 1;
+                }
+            }
+            crctable[i] = remainder;
+        }
+    }
+
+    static long crc32(sbyte[] src)
+    {
+        var crc = src.Aggregate<sbyte, long>(0xffffffff, (current, t) => (current >>> 8) ^ crctable[(current ^ t) & 0xff]);
+        return ~crc;
+    }
+    
+    private sbyte[] data;
+
+    private int pos;
+    private int bitPo = 0;
+    private Isaac? random = null;
+
+    public Packet(sbyte[] src)
+    {
+        data = src;
+        pos = 0;
+    }
+
+    public int length()
+    {
+        return data.Length;
+    }
+    
+    public int available()
+    {
+        return length() - pos;
+    }
+
+    public static Packet alloc(int type)
+    {
+        Packet? cached = null;
+        switch (type)
+        {
+            case 0 when cacheMinCount > 0:
+                cacheMinCount--;
+                cached = cacheMin.removeHead() as Packet;
+                break;
+            case 1 when cacheMidCount > 0:
+                cacheMidCount--;
+                cached = cacheMid.removeHead() as Packet;
+                break;
+            case 2 when cacheMaxCount > 0:
+                cacheMaxCount--;
+                cached = cacheMax.removeHead() as Packet;
+                break;
+        }
+
+        if (cached == null)
+            return type switch
+            {
+                0 => new Packet(new sbyte[100]),
+                1 => new Packet(new sbyte[5000]),
+                _ => new Packet(new sbyte[30000])
+            };
+        
+        cached.pos = 0;
+        return cached;
+    }
+
+    public void release()
+    {
+        pos = 0;
+        switch (data.Length)
+        {
+            case 100 when cacheMinCount < 1000:
+                cacheMin.addTail(this);
+                cacheMinCount++;
+                break;
+            case 5000 when cacheMidCount < 250:
+                cacheMid.addTail(this);
+                cacheMidCount++;
+                break;
+            case 30000 when cacheMaxCount < 50:
+                cacheMax.addTail(this);
+                cacheMaxCount++;
+                break;
+        }
+    }
+
+    public void p1isaac(int opcode)
+    {
+        data[pos++] = (sbyte) (opcode + random.nextInt());
+    }
+
+    public void p1(int value)
+    {
+        data[pos++] = (sbyte) value;
+    }
+    
+    public void p2(int value)
+    {
+        data[pos++] = (sbyte) (value >> 8);
+        data[pos++] = (sbyte) value;
+    }
+    
+    public void ip2(int value)
+    {
+        data[pos++] = (sbyte) value;
+        data[pos++] = (sbyte) (value >> 8);
+    }
+    
+    public void p3(int value)
+    {
+        data[pos++] = (sbyte) (value >> 16);
+        data[pos++] = (sbyte) (value >> 8);
+        data[pos++] = (sbyte) value;
+    }
+    
+    public void p4(int value)
+    {
+        data[pos++] = (sbyte) (value >> 24);
+        data[pos++] = (sbyte) (value >> 16);
+        data[pos++] = (sbyte) (value >> 8);
+        data[pos++] = (sbyte) value;
+    }
+    
+    public void ip4(int value)
+    {
+        data[pos++] = (sbyte) value;
+        data[pos++] = (sbyte) (value >> 8);
+        data[pos++] = (sbyte) (value >> 16);
+        data[pos++] = (sbyte) (value >> 24);
+    }
+    
+    public void p8(long value)
+    {
+        data[pos++] = (sbyte) (value >> 56);
+        data[pos++] = (sbyte) (value >> 48);
+        data[pos++] = (sbyte) (value >> 40);
+        data[pos++] = (sbyte) (value >> 32);
+        data[pos++] = (sbyte) (value >> 24);
+        data[pos++] = (sbyte) (value >> 16);
+        data[pos++] = (sbyte) (value >> 8);
+        data[pos++] = (sbyte) value;
+    }
+}
